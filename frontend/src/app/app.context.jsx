6ab1a64ad509874e46f6ce30a50d1a0d24ea445d
Abdul Ghani <ghanisheikh26@gmail.com>
2026-05-10 03:00:17 +0530

    removed a comment

--- frontend/src/app/app.context.jsx
+++ frontend/src/app/app.context.jsx
@@ -3,32 +3,6 @@ import { createContext, useState } from "react";
 export const AppContext = createContext(null);
 
 export const AppContextProvider = ({children}) => {
-
-    // messages = [
-    //     {
-    //         problem: string,
-    //         solution_1: string,
-    //         solution_2: string,
-    //         judgement: {
-    //             solution_1_score: Number,
-    //             solution_2_score: Number,
-    //             solution_1_reasoning: string,
-    //             solution_2_reasoning: string
-    //         }
-    //     }, 
-    //     {
-    //         problem: string,
-    //         solution_1: string,
-    //         solution_2: string,
-    //         judgement: {
-    //             solution_1_score: Number,
-    //             solution_2_score: Number,
-    //             solution_1_reasoning: string,
-    //             solution_2_reasoning: string
-    //         }
-    //     }
-    // ];
-
     const [messages, setMessages] = useState([]);
 
     return <AppContext.Provider value={{messages, setMessages}}>
