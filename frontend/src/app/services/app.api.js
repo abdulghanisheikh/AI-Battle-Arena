2c727b525113b58af462a72088cfe366563b9908
Abdul Ghani <ghanisheikh26@gmail.com>
2026-05-10 20:10:41 +0530

    updated some variables

--- frontend/src/app/services/app.api.js
+++ frontend/src/app/services/app.api.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-let baseURL = import.meta.env.VITE_BASE_API_URL;
+let baseURL;
+
+if(import.meta.env.VITE_NODE_ENVIRONMENT === 'development') {
+    baseURL = "http://localhost:3000";
+} else {
+    baseURL = import.meta.env.VITE_BASE_API_URL;
+}
 
 const api = new axios.create({
     baseURL,
