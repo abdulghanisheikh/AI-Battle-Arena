52adc76d1f61742375642d4d612623b8d8c92a4a
Abdul Ghani <ghanisheikh26@gmail.com>
2026-05-10 18:22:18 +0530

    added build files in backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import graphRouter from "./routes/graph.routes.js";
 import cors from "cors";
+import path, { dirname } from "path";
 import appConfig from "./configs/config.js";
+import { fileURLToPath } from "url";
 
 const app = express();
 
@@ -9,11 +11,21 @@ app.use(cors({
     origin: appConfig.FRONTEND,
     credentials: true
 }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use("/useGraph", graphRouter);
 
+// health check
 app.get("/", (req, res) => {
     res.status(200).json({ status: "ok" });
+});
+
+app.get("*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
 export default app;
